07171fef7458bcd78aa0a63361629b89c8bc9274
rmachhh <ralphmiguelchan1997@gmail.com>
2026-06-21 00:27:36 +0800

    fix: configure allowed dev origins

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+# Phase 1
+- Dev Origins
+    - Set-up allowed dev origins to make this application work on mylocal
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Allow the ngrok tunnel host to access dev resources (HMR, etc.).
-  allowedDevOrigins: ["kind-intensely-herring.ngrok-free.app"],
-};
+  allowedDevOrigins: ["pulse.chanlabs.dev", "100.110.181.84"],
+};  
 
 export default nextConfig;
